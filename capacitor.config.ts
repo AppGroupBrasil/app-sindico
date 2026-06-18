eeb3f450f43d7369a5514218505a745e9950bc86
Transplante PT <transplante@example.com>
2026-06-18 21:58:10 +0100

    Adiciona paginas de Politica de Privacidade e Exclusao de Dados; corrige appId para br.com.appsindico

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.appsindico.app',
+  appId: 'br.com.appsindico',
   appName: 'App Síndico',
   webDir: 'dist',
   server: {
